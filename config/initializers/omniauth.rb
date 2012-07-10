69ea70700029a6cff8ce451a5781e0832acb03c0
Oliver <asimov.kucevic@gmail.com>
2012-07-10 12:17:43 +0200

    Changed gemfile and omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "ARzSOraY5kZbKJ66sJoGYg", "AVuLy9zFT21jEm7OLnNcBROZWrpkTA4sO5jlWpacUKk" 
+  provider :twitter, "NgQvlZy9CO8YmDGzeDQUTA", "bSxqf4EbbvUPaowN1sEckb6F1ujBbxUDLWKXq4Q1k" 
 end
