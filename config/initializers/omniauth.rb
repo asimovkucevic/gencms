8f59bffd5806c68f890e0fe470dacbdeb9bb79c9
Oliver <asimov.kucevic@gmail.com>
2012-07-10 10:57:24 +0200

    Added keys to omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+	provider :twitter, 'ARzSOraY5kZbKJ66sJoGYg', 'AVuLy9zFT21jEm7OLnNcBROZWrpkTA4sO5jlWpacUKk'
 	# provider :facebook, 'APP_ID', 'APP_SECRET'
 	# provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
