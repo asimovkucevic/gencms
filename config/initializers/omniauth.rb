90f2cf47a82bfaac45a42542bc335830eec5a070
Oliver <asimov.kucevic@gmail.com>
2012-07-10 16:32:12 +0200

    omniauth.rb changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "NgQvlZy9CO8YmDGzeDQUTA", "bSxqf4EbbvUPaowN1sEckb6F1ujBbxUDLWKXq4Q1k" 
+  provider :twitter,  ENV['NgQvlZy9CO8YmDGzeDQUTA'], ENV['bSxqf4EbbvUPaowN1sEckb6F1ujBbxUDLWKXq4Q1k']  
+
+
+  # => "NgQvlZy9CO8YmDGzeDQUTA", "bSxqf4EbbvUPaowN1sEckb6F1ujBbxUDLWKXq4Q1k" 
 end
