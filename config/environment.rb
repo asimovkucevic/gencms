b564f5469d23f5db97e06143e1c1b6c91bbd4f32
enes <asimov.kucevic@gmail.com>
2012-07-11 14:45:04 +0200

    db migrated

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-<<<<<<< HEAD
+#<<<<<<< HEAD
 Gencms::Application.initialize!
-=======
+#=======
 Genericcmsdev::Application.initialize!
->>>>>>> 224c6a5e1d745d9bd0d5346c8eba72756e861f44
+#>>>>>>> 224c6a5e1d745d9bd0d5346c8eba72756e861f44
