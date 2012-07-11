0fad3ca48214b8702e2183efe6d39705f4007955
enes <asimov.kucevic@gmail.com>
2012-07-11 14:16:11 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gencms::Application.config.secret_token = 'ecfc958164e026b2e962ea6627e4013c0421150803ccce9049b1cc28a9a67a4eacb4afb77a9b70c821f279a52b38f783e71d0affd4cdcc685a7cafc50524fd30'
