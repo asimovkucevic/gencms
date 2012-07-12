fe1a9b4d89dd177e14f30d854224af5b82147a6e
Oliver <asimov.kucevic@gmail.com>
2012-07-12 10:12:05 +0200

    Implemented omniauth-facebook.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,  'U0wUYvkL2UOjB8BB2szdA', '7zFc8S7P1cCP9ASzpAfZQ3m17OUASnNxoJ98F6pjII'
-  provider :facebook,  '247909328661146', 'e01e8a26329a23cc1c662e3ecd0de424', {:client_options => {:ssl => {:verify => false}}}
-
-
-
-  #ENV['NgQvlZy9CO8YmDGzeDQUTA'], ENV['bSxqf4EbbvUPaowN1sEckb6F1ujBbxUDLWKXq4Q1k']  
-
-
+  provider :facebook,  '257853167662373', 'f909e4a9cbe91d2612d34f6e052a6946', {:client_options => {:ssl => {:verify => false}}}
    
 end
